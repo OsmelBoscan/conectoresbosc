1b7f6921f8db6438c3fd93e9fac6df9252837bcc
Osmel Boscan <osmelboscan@gmail.com>
2025-08-31 18:14:41 -0400

    modified:   resources/views/layouts/partials/app/footer.blade.php 	modified:   app/Http/Controllers/CheckoutController.php 	modified:   app/Http/Controllers/ProductController.php 	modified:   app/Livewire/ShippingAddresses.php 	modified:   app/Livewire/ShoppingCart.php 	modified:   resources/views/auth/login.blade.php 	modified:   resources/views/categories/show.blade.php 	modified:   resources/views/checkout/index.blade.php 	modified:   resources/views/families/show.blade.php 	modified:   resources/views/gracias.blade.php 	modified:   resources/views/livewire/filter.blade.php 	modified:   resources/views/livewire/navigation.blade.php 	modified:   resources/views/livewire/shopping-cart.blade.php 	modified:   resources/views/orders/ticket.blade.php 	modified:   resources/views/products/show.blade.php 	modified:   resources/views/shipping/index.blade.php 	modified:   resources/views/subcategories/show.blade.php 	modified:   routes/web.php

--- resources/views/layouts/partials/app/footer.blade.php
+++ resources/views/layouts/partials/app/footer.blade.php
@@ -1,64 +1,33 @@
-
-
 <footer class="bg-black dark:bg-black">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-                  <h1 class="text-white">
-                     <h1 class="text-white">
-                    <a href="/" class="inline-flex flex-col items-end">
-                        <img src="{{ asset('img/icono.jpg') }}" alt="Logo" class="h-32 w-auto mx-auto">
+            <div class="mb-6 md:mb-0 w-full flex flex-col items-center">
+                <a href="/" class="inline-flex flex-col items-center">
+                    <img src="{{ asset('img/icono.jpg') }}" alt="Logo" class="h-32 w-auto mx-auto">
+                </a>
+                <div class="flex gap-6 mt-4">
+                    <!-- Instagram -->
+                    <a href="https://instagram.com/tu_usuario" target="_blank" class="hover:text-pink-500">
+                        <svg class="h-8 w-8 text-white" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm4.25 2.25a6.25 6.25 0 1 1 0 12.5 6.25 6.25 0 0 1 0-12.5zm0 1.5a4.75 4.75 0 1 0 0 9.5 4.75 4.75 0 0 0 0-9.5zm6.13 1.12a1.13 1.13 0 1 1-2.25 0 1.13 1.13 0 0 1 2.25 0z"/>
+                        </svg>
                     </a>
-                </h1>
-                </h1>
-
-          </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
-                      </li>
-                      <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
-                      </li>
-                      <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="#" class="hover:underline">Privacy Policy</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" class="hover:underline">Conectores Bosc</a>. All Rights Reserved.
-          </span>
-          <div class="flex mt-4 sm:justify-center sm:mt-0">
-            
-             
-           
-          
-          </div>
-      </div>
+                    <!-- WhatsApp -->
+                    <a href="https://wa.me/1234567890" target="_blank" class="hover:text-green-500">
+                        <svg class="h-8 w-8 text-white" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M20.52 3.48A12.07 12.07 0 0 0 12 0C5.37 0 0 5.37 0 12c0 2.11.55 4.16 1.6 5.98L0 24l6.27-1.64A12.06 12.06 0 0 0 12 24c6.63 0 12-5.37 12-12 0-3.21-1.25-6.23-3.48-8.52zM12 22c-1.85 0-3.68-.5-5.26-1.44l-.38-.22-3.72.97.99-3.62-.25-.37A9.93 9.93 0 0 1 2 12c0-5.52 4.48-10 10-10s10 4.48 10 10-4.48 10-10 10zm5.2-7.6c-.28-.14-1.65-.81-1.9-.9-.25-.09-.43-.14-.61.14-.18.28-.7.9-.86 1.08-.16.18-.32.2-.6.07-.28-.14-1.18-.44-2.25-1.4-.83-.74-1.39-1.65-1.56-1.93-.16-.28-.02-.43.12-.57.13-.13.28-.34.42-.51.14-.17.18-.29.28-.48.09-.19.05-.36-.02-.5-.07-.14-.61-1.47-.84-2.01-.22-.53-.45-.46-.62-.47-.16-.01-.36-.01-.56-.01-.19 0-.5.07-.76.34-.26.27-1 1-.97 2.43.03 1.43 1.03 2.81 1.18 3 .15.19 2.03 3.1 4.93 4.23.69.27 1.23.43 1.65.55.69.22 1.32.19 1.81.12.55-.08 1.65-.67 1.88-1.32.23-.65.23-1.2.16-1.32-.07-.12-.25-.19-.53-.33z"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" class="hover:underline">Conectores Bosc</a>. All Rights Reserved.
+            </span>
+            <div class="flex mt-4 sm:justify-center sm:mt-0">
+                <!-- Otros iconos o enlaces aquí si lo deseas -->
+            </div>
+        </div>
     </div>
 </footer>
